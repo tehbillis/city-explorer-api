616f6a95cbde48d0ce685d86b46059c53a488841
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-03-13 00:21:59 -0700

    Complete live weather implementation

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const weatherData = require('./data/weather.json');
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const cors = require('cors'); //Cross Origin Resource Sharing
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 // Define routes and middleware here
 
@@ -16,18 +17,20 @@ app.get('/', (request, response) => {
   response.send('You made it! Welcome!');
 });
 
-app.get('/weather', (request, response) => {
-  const { lat, lon, searchQuery } = request.query;
+app.get('/weather', async (request, response) => {
+  const { lat, lon } = request.query;
+  let cityForecast = [];
 
-  console.log(Math.floor(lat * 10) / 10);
-
-  let cityWeather = new Weather(searchQuery, lat, lon);
-  console.log(cityWeather);
   try {
-    let cityForecast = cityWeather.getCityWeather().reduce((accumulator, item) => {
-      accumulator.push(new Forecast(item.datetime, item.weather.description));
-      return accumulator;
-    }, []);
+    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}&units=I`);
+
+    // Access the data array and its first element, extracting properties using optional chaining
+    weatherData?.data?.data?.forEach(day => {
+      // Create a new Forecast object using destructuring
+      const { datetime, weather, app_temp } = day;
+      const { description, icon } = weather;
+      cityForecast.push(new Forecast(datetime, description, app_temp, icon));
+    });
 
     response.send(cityForecast);
   } catch (error) {
@@ -38,33 +41,12 @@ app.get('/weather', (request, response) => {
 });
 
 class Forecast {
-  constructor(date, description) {
+  constructor(date, description, temp, icon) {
     this.date = date;
     this.description = description;
+    this.temp = temp;
+    this.icon = icon;
   }
-}
-
-class Weather {
-  constructor(city, lat, lon) {
-    this.weatherData = weatherData.find(data => (
-      data.city_name.toLocaleLowerCase() === city.toLocaleLowerCase() &&
-      truncLocation(data.lat) === truncLocation(lat) &&
-      truncLocation(data.lon) === truncLocation(lon)
-    ));
-  }
-
-  getCityName() {
-    return this.weatherData ? this.weatherData.city_name : 'City not found';
-  }
-
-  getCityWeather() {
-    return this.weatherData ? this.weatherData.data : 'City Not Found';
-  }
-}
-
-// If the location name matches the lat and lon to the 10th decimal place, we will accept it as being the right location for now.
-function truncLocation (location) {
-  return Math.floor(location * 10) / 10;
 }
 
 app.get('*', (request, response) => {
