f5cf4a4555394d62e7dea68a78e79f31925765d2
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-03-14 00:30:56 -0700

    Added dynamic days of the week to match the wether forecast

--- server.js
+++ server.js
@@ -23,14 +23,29 @@ app.get('/weather', async (request, response) => {
 
   try {
     const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}&units=I&days=3`);
+    let dayOfTheWeek = '';
 
     // Access the data array and its first element, extracting properties using optional chaining
     weatherData?.data?.data?.forEach(day => {
       // Create a new Forecast object using destructuring
       const { datetime, weather, app_temp } = day;
       const { description, icon } = weather;
-      cityForecast.push(new Forecast(datetime, description, app_temp, icon));
+
+      // Find day of the week
+      const date = new Date(datetime);
+      const dateDay = date.getDay();
+      const dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+      if (getTodaysDate() === datetime) { // TODO: Create function to find out if today is today
+        dayOfTheWeek = 'Today';
+      } else {
+        dayOfTheWeek = dayNames[dateDay];
+      }
+
+      cityForecast.push(new Forecast(datetime, dayOfTheWeek, description, app_temp, icon));
     });
+
+    console.log(cityForecast);
 
     response.send(cityForecast);
   } catch (error) {
@@ -41,12 +56,28 @@ app.get('/weather', async (request, response) => {
 });
 
 class Forecast {
-  constructor(date, description, temp, icon) {
+  constructor(date, day, description, temp, icon) {
     this.date = date;
+    this.day = day;
     this.description = description;
     this.temp = temp;
     this.icon = icon;
   }
+}
+
+function getTodaysDate () {
+  const currentDate = new Date();
+
+    // Get the year, month, and day
+    const year = currentDate.getFullYear();
+    // JavaScript months are zero-based, so add 1 to get the correct month
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding padding if month is a single digit
+    const day = String(currentDate.getDate()).padStart(2, '0'); // Adding padding if day is a single digit
+
+    // Form the date string in "YYYY-MM-DD" format
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
 }
 
 app.get('*', (request, response) => {
